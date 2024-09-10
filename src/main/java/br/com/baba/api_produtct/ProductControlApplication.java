3b87572e91fde6e58c2f117b6cb25d8679473dae
lbaba <leobaba111@gmail.com>
2024-09-09 23:04:20 -0300

    first commit

---
+++ src/main/java/br/com/baba/api_produtct/ProductControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.baba.api_produtct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductControlApplication.class, args);
+	}
+
+}
