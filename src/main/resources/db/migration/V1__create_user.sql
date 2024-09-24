8bffdb19fec595ff5736a7e670dc13fa3a155257
lbaba <leobaba111@gmail.com>
2024-09-23 23:38:07 -0300

    feat: add migration to create table users

---
+++ src/main/resources/db/migration/V1__create_user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(10) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    role VARCHAR(10) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    creation_date DATE NOT NULL
+);
